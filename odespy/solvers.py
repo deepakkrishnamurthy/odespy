6a99d9a99c330760d0331f12a8d1130eea44ec99
Rajesh Singh <rajeshrinet@gmail.com>
2019-02-12 12:13:56 +0000

    fix solvers for 3

--- odespy/solvers.py
+++ odespy/solvers.py
@@ -935,7 +935,7 @@ class Solver:
             f_name = self.users_f.__name__
             if f_name == '<lambda>':
                 f_name = 'lambda u, t: ...'
-            args.append('f=%s' % f_name)
+        args.append('f=%s' % f_name)
 
         # form all parameters
         for name in self._parameters:
@@ -961,9 +961,9 @@ class Solver:
             if hasattr(self.users_jac, '__name__'):     # plain function?
                 f_name = self.users_jac.__name__
             if f_name == '<lambda>':   # lambda function
-            #f_name = 'lambda u, t: ...'
-            f_name = 'lambda'
-        else:                                       # class instance
+                #f_name = 'lambda u, t: ...'
+                f_name = 'lambda'
+            else:                                       # class instance
                 f_name = self.users_jac.__class__.__name__
             args.append('jac=%s' % f_name)
 
@@ -1161,23 +1161,23 @@ class Solver:
             or (not np.asarray(
             # all items in self.t should be numbers
             [isinstance(t, (int,float)) for t in self.t]).all()):
-                raise TypeError, \
+                raise TypeError( \
                     'solve: time_points(=%s) is not a proper '\
-                    'sequence of real numbers' % str(self.t)
+                    'sequence of real numbers' % str(self.t))
 
         # self.t should be supplied in an asscending/descending order
         t_sorted = sorted(self.t, reverse=self.t[0] > self.t[-1])
         if list(self.t) != list(t_sorted):
-            raise ValueError, \
+            raise ValueError( \
                 'time_points(=%s) is not provided in an ascending/descending'\
-                ' order!' % str(self.t)
+                ' order!' % str(self.t))
 
         # Test whether all required parameters are provided
         for arg in self._required_parameters:
             if not hasattr(self, arg):
-                raise ValueError,\
+                raise ValueError(\
                     '"%s" has to be input as required parameter(s) for '\
-                    'solver %s.' % (arg,self.__class__.__name__)
+                    'solver %s.' % (arg,self.__class__.__name__))
         return True
 
     def switch_to(self, solver_target, print_info=False, **kwargs):
@@ -1220,10 +1220,10 @@ class Solver:
                 # Convert string to class name
                 solver_target = getattr(odespy, solver_target)
         except:
-            raise ValueError, error_message
+            raise ValueError( error_message)
 
         if not solver_target.__name__ in list_all_solvers():
-            raise ValueError, error_message
+            raise ValueError( error_message)
 
 
 
@@ -1334,9 +1334,9 @@ class Solver:
                         found = hasattr(self, arg)
                         arg_print = arg
                     if not found:
-                        raise ValueError,'''\
+                        raise ValueError('''\
         Error! Unsufficient input!
-        %s must be set when %s is %s!''' % (arg_print,name,value)
+        %s must be set when %s is %s!''' % (arg_print,name,value))
         return True
 
     def has_u_t_all(self):
@@ -1507,7 +1507,7 @@ class RK2(Solver):
         dt = t[n+1] - t[n]
         K1 = dt*f(u[n], t[n])
         K2 = dt*f(u[n] + 0.5*K1, t[n] + 0.5*dt)
-    u_new = u[n] + K2
+        u_new = u[n] + K2
         return u_new
 
 
@@ -2013,7 +2013,7 @@ class odefun_sympy(Solver):
             import sympy
             self.sympy = sympy
         except ImportError:
-            raise ImportError,'sympy is not installed - needed for sympy_odefun'
+            raise ImportError('sympy is not installed - needed for sympy_odefun')
 
     def initialize_for_solve(self):
         # sympy.odefun requires f(t, u), not f(u, t, *args, **kwargs)
@@ -2908,7 +2908,7 @@ class odelab(Adaptive):
     solvers = 'ExplicitEuler ExplicitTrapezoidal ImplicitEuler RungeKutta34 RungeKutta4 SymplecticEuler ImplicitEuler LDIRK343 LobattoIIIA LobattoIIIB LobattoIIIC LobattoIIICs LobattoIIID MultiRKDAE RKDAE RadauIIA RungeKutta Spark Spark2 Spark3 Spark4 AdamsBashforth AdamsBashforth1 AdamsBashforth2 AdamsBashforth2e Butcher Butcher1 Butcher3 ExplicitGeneralLinear GeneralLinear Heun Kutta Kutta38 Kutta4 McLachlan NonHolonomicEnergy NonHolonomicEnergy0 NonHolonomicEnergyEMP NonHolonomicLeapFrog SymplecticEuler ABLawson ABLawson2 ABLawson3 ABLawson4 ABNorset4 Exponential GenLawson45 HochOst4 Lawson4 LawsonEuler Phi Polynomial RKMK4T'.split()
     DAE_solvers = 'SymplicticEuler MultiRKDAE RKDAE Spark Spark2 Spark3 Spark4'.split()
     not_valid = 'SymplecticEuler LDIRK343 LobattoIIIA LobattoIIIB LobattoIIIC LobattoIIICs LobattoIIID MultiRKDAE RKDAE RadauIIA RungeKutta Spark Spark2 Spark3 Spark4 AdamsBashforth AdamsBashforth2 AdamsBashforth2e Butcher Butcher1 Butcher3 ExplicitGeneralLinear GeneralLinear Kutta McLachlan NonHolonomicEnergy NonHolonomicEnergy0 NonHolonomicEnergyEMP NonHolonomicLeapFrog SymplecticEuler ABLawson ABLawson2 ABLawson3 ABLawson4 ABNorset4 Exponential GenLawson45 HochOst4 Lawson4 LawsonEuler Phi Polynomial RKMK4T'.split()
-    solvers = [solver for solver in solvers if not solver in not_valid]
+    solvers = [solver for solver in solvers if not solver in 'SymplecticEuler LDIRK343 LobattoIIIA LobattoIIIB LobattoIIIC LobattoIIICs LobattoIIID MultiRKDAE RKDAE RadauIIA RungeKutta Spark Spark2 Spark3 Spark4 AdamsBashforth AdamsBashforth2 AdamsBashforth2e Butcher Butcher1 Butcher3 ExplicitGeneralLinear GeneralLinear Kutta McLachlan NonHolonomicEnergy NonHolonomicEnergy0 NonHolonomicEnergyEMP NonHolonomicLeapFrog SymplecticEuler ABLawson ABLawson2 ABLawson3 ABLawson4 ABNorset4 Exponential GenLawson45 HochOst4 Lawson4 LawsonEuler Phi Polynomial RKMK4T'.split()]
 
     __doc__ = __doc__ % (str(solvers)[1:-1])
 
@@ -2943,7 +2943,7 @@ class odelab(Adaptive):
             self.odelab_schemes = schemes
 
         except ImportError:
-            raise ImportError,'The odelab software is not installed - needed for class odelab'
+            raise ImportError('The odelab software is not installed - needed for class odelab')
 
     def initialize_for_solve(self):
         # odelab requires f(t, u), not f(u, t, *args, **kwargs)
@@ -2992,9 +2992,11 @@ class odelab(Adaptive):
             self.solver.initialize(self.U0)
             # Solve problem in odelab
             self.solver.run(self.t[-1])
-        except Exception, e:
-            msg = 'odelab scheme %s did not work:\n%s' % \
-                  (self.odelab_solver, e)
+        except:
+            print ('odesolver did not work')
+            # Exception('error')
+            #msg = 'odelab scheme %s did not work:\n%s' % \
+            #      (self.odelab_solver, e))
             #print msg
             #print 'returning solution self.u as None'
             #return None, self.t
